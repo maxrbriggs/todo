ba332ac9c913c59d8d53a967a6d0334485928b4d
Max Briggs <max@maxbriggs.org>
2020-08-12 13:40:24 -0700

    Remove debug printing

--- src/helpers.c
+++ src/helpers.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "helpers.h"
 
 void no_args(char *tomorrow_iso, FILE *todo_file) {
 	int string_found = 0;
@@ -5,22 +7,15 @@ void no_args(char *tomorrow_iso, FILE *todo_file) {
 	char *line = NULL;
 	size_t line_length = 0;
 
-	int fprintf_return;
-
 	while (getline(&line, &line_length, todo_file) != -1) {
 		if (!strcmp(line, tomorrow_iso)) {
-			printf("Tomorrow string found!\n");
 			string_found = 1;
 			break;
 		}
 	}
 
 	if (!string_found) {
-		printf("Tomorrow not found. Adding it.\n");
-
-		fprintf_return = fprintf(todo_file, "%s\n", tomorrow_iso);
-
-		printf("fprintf error: %i\n", fprintf_return);
+		fprintf(todo_file, "%-s", tomorrow_iso);
 	}
 }
 
