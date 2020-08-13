399388f5bd7620c4365a1b0358d780f520da7225
Max Briggs <max@maxbriggs.org>
2020-08-12 17:10:39 -0700

    Add find_line

--- src/helpers.c
+++ src/helpers.c
@@ -14,8 +14,25 @@ void no_args(char *tomorrow_iso, FILE *todo_file) {
 		}
 	}
 
-	if (!string_found) {
+	if (!string_found)
 		fprintf(todo_file, "%-s", tomorrow_iso);
-	}
 }
 
+void find_line(char *today_iso, char *tomorrow_iso, FILE *todo_file) {
+	int today_line = 0;
+	int tomorrow_line = 0;
+
+	char *line = NULL;
+	size_t line_length = 0;
+
+	int line = 0;
+
+	while (getline(&line, &line_length, todo_file) != -1) {
+		++line;
+
+		if (!strcmp(line, today_iso))
+			today_line = line;
+		else if (!strcmp(line, tomorrow_iso))
+			tomorrow_line = line;
+	}
+}
